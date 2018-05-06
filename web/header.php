efd5103570361e15f10eab62203cd4fd3d26fa9a
minzoe <min16009@byui.edu>
2018-05-05 21:46:28 -0600

    adding style

--- web/header.php
+++ web/header.php
@@ -1,8 +1,7 @@
 <header>
-    <h1>Zoe Miner | Web Engineering</h1>
     <nav  class="navbar navbar-inverse">
     <ul class="nav navbar-nav">
-        <li><a href='home.php' <?php if($current=="home") { ?>  class="active"   <?php   }  ?>>Home</a></li>
-        <li><a href='directory.php' <?php if($current=="directory") { ?>  class="active"   <?php   }  ?>>About Us</a></li>
+        <li <?php if($current=="home") { ?>  class="active"   <?php   }  ?> ><a href='home.php'>Home</a></li>
+        <li <?php if($current=="directory") { ?>  class="active"   <?php   }  ?> ><a href='directory.php'>About Us</a></li>
     </nav>
 </header>
