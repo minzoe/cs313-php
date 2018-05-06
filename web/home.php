1657231e3d10c60bb7d65f9cddd8be3ddc6f07c3
minzoe <min16009@byui.edu>
2018-05-05 21:28:54 -0600

    creating home

---
+++ web/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+session_start();
+$current = 'home';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel='stylesheet' href='main.css'>
+    <title>Home</title>
+</head>
+<body>
+    <?php include 'header.php' ?>
+    <h2>Home</h2>
+    <p>Welcome, I am Zoe Miner. This site will be my directory for my projects in my Web Engineering Class.</p>
+</body>
+</html>
