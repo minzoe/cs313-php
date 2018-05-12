f36aef8b1bd278441f070519eaf9d56f374c87e7
minzoe <min16009@byui.edu>
2018-05-12 17:09:20 -0600

    testing

--- web/shoppingCart/setCart.php
+++ web/shoppingCart/setCart.php
@@ -2,6 +2,8 @@
 
 $q = filter_input(INPUT_REQUEST, 'q', FILTER_SANITIZE_STRING);
 
-array_push($_SESSION['cart'], $item)
-                
+array_push($_SESSION['cart'], $item);
+
+$message = item + " has been added to the cart";
+echo "<script type='text/javascript'>alert('$message');</script>";
 ?>
