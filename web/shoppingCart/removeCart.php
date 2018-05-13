b484c08ce51578dcc2b17ab034d4efed23893043
minzoe <min16009@byui.edu>
2018-05-12 18:44:06 -0600

    testing

--- web/shoppingCart/removeCart.php
+++ web/shoppingCart/removeCart.php
@@ -2,10 +2,6 @@
 session_start();
 $item = $_POST["item"];
 
-$cart = $_SESSION['cart'];
-foreach ($cart as $list) {
-    if ($list === $item) {
-        unset($_SESSION['cart'][$list]);
-        }
-        }
-        ?>
+unset($_SESSION['cart'][$list]);
+    
+?>
