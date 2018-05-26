b92e3f1bc77ab2dfdf25e0f52f48de0ae3f535f4
minzoe <min16009@byui.edu>
2018-05-26 15:22:10 -0600

    fixing

--- web/games/index.php
+++ web/games/index.php
@@ -26,7 +26,7 @@ if (isset($_POST['Search'])) {
     $time = filter_input(INPUT_POST, 'time', FILTER_SANITIZE_NUMBER_INT);
     $decks = filter_input(INPUT_POST, 'decks', FILTER_SANITIZE_NUMBER_INT);
     $relaxed = filter_input(INPUT_POST, 'relax', FILTER_VALIDATE_BOOLEAN);
-    echo 'start qurery'
+    echo 'start qurery';
     $query = "SELECT title, description FROM games WHERE numOfPlayers = :players AND timeLength = :time AND numOfDecks = :decks AND relaxed = :realaxed";
     $stmt = $db->prepare($query);
     $stmt->bindValue(":players", $players, PDO::PARAM_INT);
