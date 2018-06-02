e68454c7dbdd352e6b4fff423f6b068d36a8cef6
minzoe <min16009@byui.edu>
2018-06-02 14:02:51 -0600

    fixing

--- web/games/index.php
+++ web/games/index.php
@@ -54,7 +54,7 @@ if (isset($_POST['Search'])) {
             <?php 
                 if(isset($_POST['Search'])) {
                     forEach ($searched as $game) {
-                        echo "<div class='panel panel-default> <h2 class='panel-heading'>$game[title]</h2> <p class='panel-body'>$game[description]</p>";
+                        echo "<div class='panel panel-default> <div class='panel-heading'>$game[title]</div> <div class='panel-body'>$game[description]</div>";
                         }
                 } else {
                     forEach ($allGames as $game) {
